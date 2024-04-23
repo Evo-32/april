a555179e3cf74d049d0b5b702c40a757998e0da2
Evo-32 <vodospearl@gmail.com>
2024-04-23 10:17:21 +0200

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./component/Home"
-import About from "./component/About"
-import Contact from "./component/Contact"
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
+import Contact from "./component/Contact";
 
 const App = () => {
   return (
-    <Router >
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/contact' element={<Contact />}/>
-      </Routes>
-    </Router>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <ul>
+        <link to={"/"}>Home</link>
+        <link to={"/about"}>About</link>
+        <link to={"/contact"}>Contact</link>
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
